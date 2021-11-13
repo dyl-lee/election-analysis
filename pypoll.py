4f937c005ebb4c7d44ac8a91c8d4d9af970d16b7
dyl-lee <dylanlee1701@gmail.com>
2021-11-13 18:25:36 -0500

    added pypoll.py

---
+++ pypoll.py
@@ -0,0 +1,11 @@
+# Pseudocode
+# Total number of votes cast
+# A complete list of candidates who received votes
+# Total number of votes each candidate received
+# Percentage of votes each candidate won
+# The winner of the election based on popular vote
+
+# dependencies
+import datetime as dt
+now = dt.datetime.now()
+print("It is currently", now)
