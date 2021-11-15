6863b702c60d1e5de88c72f42004931b2de16e8c
dyl-lee <dylanlee1701@gmail.com>
2021-11-15 00:11:49 -0500

    updated code to open election_data

--- pypoll.py
+++ pypoll.py
@@ -9,3 +9,14 @@
 import datetime as dt
 now = dt.datetime.now()
 print("It is currently", now)
+
+# assign variable for file to load and the path as value. We are telling computer to get .csv from Resources folder
+file_to_load = 'Resources/election_results.csv'
+
+# open file for reading. Note, variable doesn't need quotes but the mode does
+with open(file_to_load) as election_data:
+
+# To do: perform analysis
+    print(election_data)
+# Close file. Note the syntax for closing is like a method? Closing the file ensures the data is preserved (if read) and any data is stored (if written)
+election_data.close()
